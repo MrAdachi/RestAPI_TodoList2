e8d755690e80bd0e7837dc9699a143681fac9a83
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-06-27 16:49:59 +0900

    [1] TodoListAPI(DB連携)の作成開始

---
+++ src/main/java/com/original/RestAPI_TodoList2/RestApiTodoList2Application.java
@@ -0,0 +1,13 @@
+package com.original.RestAPI_TodoList2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTodoList2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTodoList2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/original/RestAPI_TodoList2/RestApiTodoList2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.original.RestAPI_TodoList2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTodoList2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
