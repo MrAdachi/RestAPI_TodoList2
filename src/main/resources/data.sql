a4235b01326fd137639194058dbdff0a7e5efbc7
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-06-28 15:17:56 +0900

    Spring Boot、MyBatisを使用したデータベース連携を活用したRESTful TODOリストAPIの構築 下記要件の機能実装 　・TODOの編集機能（PUTエンドポイント、ID指定） 　・余分な要素の追加（作成日時、更新日時） 　※一部pushし忘れがあったため、その分をpush

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO todoitems(title, status, details) VALUES('家電購入', '未着手', 'テレビ購入');
+INSERT INTO todoitems(title, status, details) VALUES('家族旅行', '未着手', '箱根の旅館で1泊する');
+INSERT INTO todoitems(title, status, details) VALUES('家族旅行', '進行中', '東京ディズニーランドで2泊する');
+INSERT INTO todoitems(title, status, details) VALUES('病院', '未着手', '歯医者に行く');
+INSERT INTO todoitems(title, status, details) VALUES('病院', '完了', '皮膚科に行く');
